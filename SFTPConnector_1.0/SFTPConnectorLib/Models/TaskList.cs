77998b2e4ac77f2bcbe6de1916e9cc1f8132fc9b
Nerower <p0sirega121234@gmail.com>
2021-02-07 17:42:31 +0300

    SFTPConnectorLib: TaskList model added

---
+++ SFTPConnector_1.0/SFTPConnectorLib/Models/TaskList.cs
@@ -0,0 +1,36 @@
+﻿using SFTPConnectorLib.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SFTPConnectorLib.Models
+{
+    public class TaskList
+    {
+        private List<FileData> tasks;
+
+        public TaskList()
+        {
+            tasks = new List<FileData>();
+        }
+
+        public IEnumerable<FileData> GetTasks()
+        {
+            return tasks;//EMPTY LIST IF IT WASNT UPLOADED ALREADY
+        }
+        public void SetTasks(string taskFilePath)
+        {
+            //загрузка данных из csv-файла в Tasks
+            var taskReader = new CsvTaskReader(taskFilePath);
+            tasks = taskReader.Read().ToList();
+        }
+
+        public void UpdateLocalPath(string basePath)
+        {
+            foreach (FileData fileData in tasks)
+            {
+                fileData.SetLocalPath(basePath);
+            }
+        }
+    }
+}
