1097a48f4ecd5ad84cad9a9bd0019158979a87af
Nerower <p0sirega121234@gmail.com>
2021-02-08 02:20:47 +0300

    Models: FileSystemGuide user control added

---
+++ SFTPConnector_1.0/UserControls/FileSystemGuide.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace SFTPConnector_1._0.UserControls
+{
+    partial class FileSystemGuide
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // FileSystemGuide
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "FileSystemGuide";
+            this.Size = new System.Drawing.Size(93, 66);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ SFTPConnector_1.0/UserControls/FileSystemGuide.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SFTPConnector_1._0.UserControls
+{
+    public partial class FileSystemGuide : UserControl
+    {
+        public FileSystemGuide()
+        {
+            InitializeComponent();
+        }
+    }
+}
