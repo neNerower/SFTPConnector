68ea2db7d7745a327d4e2719b68f896fbad25161
Nerower <p0sirega121234@gmail.com>
2021-02-07 19:14:50 +0300

    added TaskTable user control based on DataGridView control

---
+++ SFTPConnector_1.0/UserControls/TaskTable.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SFTPConnector_1._0.UserControls
+{
+    partial class TaskTable
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.taskDataTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.taskDataTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // taskDataTable
+            // 
+            this.taskDataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.taskDataTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.taskDataTable.Location = new System.Drawing.Point(0, 0);
+            this.taskDataTable.Name = "taskDataTable";
+            this.taskDataTable.RowHeadersWidth = 51;
+            this.taskDataTable.RowTemplate.Height = 24;
+            this.taskDataTable.Size = new System.Drawing.Size(608, 324);
+            this.taskDataTable.TabIndex = 0;
+            // 
+            // TaskTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.taskDataTable);
+            this.Name = "TaskTable";
+            this.Size = new System.Drawing.Size(608, 324);
+            ((System.ComponentModel.ISupportInitialize)(this.taskDataTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView taskDataTable;
+    }
+}
---
+++ SFTPConnector_1.0/UserControls/TaskTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SFTPConnectorLib.Models;
+
+namespace SFTPConnector_1._0.UserControls
+{
+    public partial class TaskTable : UserControl
+    {
+        private readonly TaskList _taskList = new TaskList();
+
+        public TaskTable()
+        {
+            InitializeComponent();
+
+            taskDataTable.DataSource = _taskList;
+        }
+
+    }
+}
